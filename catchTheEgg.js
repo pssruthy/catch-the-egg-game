080fd35d01fd73b21543068d6e46dbaeb7041f0a
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2019-12-08 20:30:52 +0530

    Added user instructions and color for text
    What changes you have made?
    --->Added user instructions
    
    Why this change is necessary?
    --->User can understand what the game is

--- catchTheEgg.js
+++ catchTheEgg.js
@@ -1,13 +1,12 @@
+'use strict';
 const process = require('process');
+const { red, greenBright, yellow } = require('chalk');
 const { stdout, stdin } = process;
 stdin.setRawMode(true);
+const readlineSync = require('readline-sync');
 
 stdin.setEncoding('utf8');
-const row = stdout.rows;
 const eggsPositionInfo = [];
-const gameProgress = { saved: 0, chances: 5 };
-const bucketPosition = { row: row - 3, leftEnd: 5, rightEnd: 15 };
-const bucket = '🗑🗑🗑🗑🗑🗑🗑🗑🗑🗑';
 const egg = '🥚';
 
 const generateNewEgg = function() {
@@ -15,20 +14,26 @@ const generateNewEgg = function() {
   const column = stdout.columns;
   newEgg.egg = egg;
   newEgg.x = Math.floor(Math.random() * column);
-  newEgg.y = 2;
+  newEgg.y = 3;
   return newEgg;
 };
 
 const dropNewEgg = function() {
-  eggsPositionInfo.push(generateNewEgg());
+  const newEgg = generateNewEgg();
+  eggsPositionInfo.push(newEgg);
+};
+
+const displayScreen = function() {
+  displayEggs();
+  displayBucket();
+  displayGrass();
 };
 
 const displayEggs = function() {
-  stdout.moveCursor(0, 1);
-  stdout.clearScreenDown();
+  console.clear();
   eggsPositionInfo.forEach(eggPosition => {
     stdout.cursorTo(eggPosition.x, eggPosition.y);
-    console.log('🥚');
+    stdout.write(eggPosition.egg);
     eggPosition.y += 1;
   });
 };
@@ -38,45 +43,51 @@ const displayGrass = function() {
   stdout.write('🌵'.repeat(stdout.columns / 2));
 };
 
+const row = stdout.rows;
+const bucketPosition = { row: row - 3, leftEnd: 5, rightEnd: 15 };
+const bucket = '🗑🗑🗑🗑🗑🗑🗑🗑🗑🗑';
+
 const displayBucket = function() {
   stdout.cursorTo(bucketPosition.leftEnd, bucketPosition.row);
   stdout.write(bucket);
 };
 
+const gameProgress = { savedEggs: 0, chancesLeft: 5 };
+
 const calculateMarks = function() {
   let eggInfoIndex = 0;
   const bucketRow = bucketPosition.row;
   eggsPositionInfo.forEach(egg => {
-    if (egg.y == bucketRow) {
+    const isEggReachedGround = egg.y == bucketRow;
+
+    if (isEggReachedGround) {
       const { leftEnd, rightEnd } = bucketPosition;
-      if (egg.x >= leftEnd && egg.x <= rightEnd) {
-        gameProgress.saved += 1;
-      } else {
-        gameProgress.chances -= 1;
-        eggsPositionInfo.egg = '🐣';
-      }
+      const isPlayerSaveTheEgg = egg.x >= leftEnd && egg.x <= rightEnd;
+
+      if (isPlayerSaveTheEgg) {
+        gameProgress.savedEggs += 1;
+      } else gameProgress.chancesLeft -= 1;
       eggsPositionInfo.splice(eggInfoIndex, 1);
     }
     eggInfoIndex++;
   });
 };
 
-const setEnv = function() {
+const setEnvironment = function() {
   console.clear();
-  displayEggs();
-  displayBucket();
-  displayGrass();
+  displayScreen();
   calculateMarks();
   isGameOver();
+
   const row = stdout.rows;
   stdout.cursorTo(1, row);
-  stdout.write(`Saved eggs :${gameProgress.saved}`);
+  stdout.write(`${greenBright(`Saved eggs :${gameProgress.savedEggs}`)}`);
   stdout.cursorTo(30, row);
-  stdout.write(`Remaining Eggs :${gameProgress.chances}`);
+  stdout.write(`${red(`Remaining Eggs :${gameProgress.chancesLeft}`)}`);
 };
 
 const isGameOver = function() {
-  if (gameProgress.chances == 0) {
+  if (gameProgress.chancesLeft == 0) {
     quitGame();
   }
 };
@@ -84,7 +95,8 @@ const isGameOver = function() {
 const displayMarks = function() {
   console.clear();
   stdout.cursorTo(1, 1);
-  stdout.write(`You saved : ${gameProgress.saved}\n`);
+  stdout.write(`${yellow(`GAME OVER\n\n`)}`);
+  stdout.write(`${greenBright(`You saved : ${gameProgress.savedEggs}\n`)}`);
 };
 
 const quitGame = function() {
@@ -103,19 +115,44 @@ const moveBucket = function(keyStroke) {
   const keyStrokes = ['f', 'j'];
   const movementDir = keyStrokes.indexOf(keyStroke);
   if (movementDir != -1) {
-    if (movementDir === 0 && bucketPosition.leftEnd >= 1) {
+    const isMovementToLeft = movementDir === 0 && bucketPosition.leftEnd >= 1;
+
+    if (isMovementToLeft) {
       bucketPosition.leftEnd -= 3;
       bucketPosition.rightEnd -= 3;
     }
-    if (movementDir === 1 && bucketPosition.rightEnd <= stdout.columns) {
+    const isMovementToRight =
+      movementDir === 1 && bucketPosition.rightEnd <= stdout.columns;
+
+    if (isMovementToRight) {
       bucketPosition.leftEnd += 3;
       bucketPosition.rightEnd += 3;
     }
   }
 };
+const getInstructionsForUser = function() {
+  const welcome = 'Welcome to Catch The Egg game🐣\n';
+
+  let whatToDo = 'What is the "Catch The Egg game"\n';
+  whatToDo += 'Eggs will fall from the top 🥚\n';
+  whatToDo += 'you are here to save the egg by moving the bowl.\n';
+
+  const chances = `You will get 5 chances to continue the game`;
+
+  let direction = 'You can move the bowl by clicking f and j keys.\n';
+  direction += 'CLick "f" for left and "j" for right';
+
+  const startTheGame = 'Click enter to start the game\n ALL THE BEST';
+
+  const instructions = [welcome, whatToDo, direction, startTheGame].join('\n');
+  return instructions;
+};
 
 const main = function() {
-  setInterval(setEnv, 300);
+  console.clear();
+  readlineSync.question(yellow(getInstructionsForUser()));
+  stdin.resume();
+  setInterval(setEnvironment, 300);
   setInterval(dropNewEgg, 4000);
   stdin.on('data', getGameStatus);
 };
